9c4f31b4459458b2af38c7f88038c2b9d4f6a957
mark-marshall <mark.marshallgp@gmail.com>
2019-09-17 13:50:06 +0100

    Moved checkout.py strings to utils.py

--- unidays/checkout.py
+++ unidays/checkout.py
@@ -1,19 +1,20 @@
 from unidays import UnidaysDiscountChallenge
 from config import pricingRules, deliveryRules
 from unidays_run import RunUnidays
+from utils import userInputs, errors
 
 # initialize userInput
 userInput = None
 # loop start
 while not userInput == '2':
     # provide user option for new price calculation or exit
-    userInput = str(input("Options: [1] Calculate a new price, [2] Exit\nEnter option: "))
+    userInput = str(input(userInputs['selection']))
     # check that the user gave a valid input
     if not (userInput == '1' or userInput == '2'):
-        print("ERROR: You entered an invalid option.")
+        print(errors['invalidSelection'])
     # check if user selected new price calculation
     elif userInput == '1':
-        itemsToAdd = list(str(input("Enter items to be added: ")).upper())
+        itemsToAdd = list(str(input(userInputs['items'])).upper())
         # create a new instance of UnidaysDiscountChallenge
         checkout = UnidaysDiscountChallenge(pricingRules,deliveryRules)
         # create a new instance of RunUnidays
