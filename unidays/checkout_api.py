1483faa31b5a62219b6f96da01f150711cf4d37b
mark-marshall <mark.marshallgp@gmail.com>
2019-09-17 11:48:58 +0100

    Amended documentation on checkout_api

--- unidays/checkout_api.py
+++ unidays/checkout_api.py
@@ -6,71 +6,75 @@ from config import pricingRules, deliveryRules
 from unidays_run import RunUnidays
 from config_api import errors, statusCodes
 
+# create Flask app
 app = Flask(__name__)
+# enable CORS
 CORS(app)
 
+# sanity check endpoint
 """
 @type: [GET]
 @path: '/'
 @params: none
 @query: none
 @body: none
-@response:
-Success (status 200):
-{
-    "Mesage": "Server is live."
-}
+@responses:
+    Success (status 200):
+    {
+        "Mesage": "Server is live."
+    }
 """
 @app.route('/', methods=['GET'])
 def server_check():
-    # return success message for sanity check endpoint
+    # return success message for sanity check
     return ({'Message': 'Server is live.'}, statusCodes['success'])
 
+# price endpoint
 """
 @type: [POST]
 @path: '/price'
 @params: none
 @query: none
 @body: {"items": "bbbbccccz"}
-@response: 
-Success (status 200)
-{
-    "Basket": {
-        "B": {
-            "finalCost": 40,
-            "itemSavings": 8,
-            "quantity": 4,
-            "unitPrice": 12
-        },
-        "C": {
-            "finalCost": 14,
-            "itemSavings": 2,
-            "quantity": 4,
-            "unitPrice": 4
-        }
-    },
-    "DeliveryCharge": 0,
-    "Errors": {
-        "Item Z": [
-            {
-                "noPricingRules": "ERROR: An item was passed that has 
-                not been included in the pricing rules."
+@responses: 
+    Success (status 200)
+    {
+        "Basket": {
+            "B": {
+                "finalCost": 40,
+                "itemSavings": 8,
+                "quantity": 4,
+                "unitPrice": 12
+            },
+            "C": {
+                "finalCost": 14,
+                "itemSavings": 2,
+                "quantity": 4,
+                "unitPrice": 4
             }
-        ]
-    },
-    "Savings": 10,
-    "Total": 62
-}
-Failure (status 400):
-{
-    "Message": "ERROR: An incorrect JSON body was passed with the request. 
-    Please provide a JSON body with an items key and list of items e.g. 
-    {items: abc}."
-}
+        },
+        "DeliveryCharge": 0,
+        "Errors": {
+            "Item Z": [
+                {
+                    "noPricingRules": "ERROR: An item was passed that has 
+                    not been included in the pricing rules."
+                }
+            ]
+        },
+        "Savings": 10,
+        "Total": 62
+    }
+    Failure (status 400):
+    {
+        "Message": "ERROR: An incorrect JSON body was passed with the request. 
+        Please provide a JSON body with an items key and list of items e.g. 
+        {items: abc}."
+    }
 """
 @app.route('/price', methods=['POST'])
 def calculate_price():
-    # save the requested item to a variable
+    # save the JSON request body
     itemsSubmitted = request.get_json()
     # check the request contains an items key
     if 'items' not in itemsSubmitted:
@@ -83,6 +87,7 @@ def calculate_price():
     run = RunUnidays(checkout, itemsToAdd)
     # return the response from RunUnidays
     return (run.All(), statusCodes['success'])
-    
+
+# serve the app locally on port 5000   
 if __name__ == '__main__':
     app.run(port=5000)
