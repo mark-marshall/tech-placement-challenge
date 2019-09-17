756ce198b77747f495611360fed8ea60eb9f37ff
mark-marshall <mark.marshallgp@gmail.com>
2019-09-17 11:50:04 +0100

    Amended comments on checkout.py

--- unidays/checkout.py
+++ unidays/checkout.py
@@ -2,12 +2,13 @@ from unidays import UnidaysDiscountChallenge
 from config import pricingRules, deliveryRules
 from unidays_run import RunUnidays
 
-# initialize userInput for conditional while loop
+# initialize userInput
 userInput = None
-# loop starts here
+# loop start
 while not userInput == '2':
     # give option for new price calculation or exit
     userInput = str(input("Options: [1] Calculate a new checkout, [2] Exit\nEnter option: "))
+    # check if user selected new price calculation
     if userInput == '1':
         itemsToAdd = list(str(input("Enter items to be added: ")).upper())
         # create a new instance of UnidaysDiscountChallenge
